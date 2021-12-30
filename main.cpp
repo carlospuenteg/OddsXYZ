c655699381af6e5fe51d529f80735045f625c457
Fisherman386 <carlospuenteg@gmail.com>
2021-12-30 15:54:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main()
 {
     float odds;
-    float oddOdds = 1;
     int tries, r;
     printf("Odds of X (with Y odds) happening after Z tries\n");
     do {
@@ -17,12 +16,30 @@ int main()
         cout << "Tries: ";
         cin >> tries;
         cout << "Odds: " << pow(1-odds,tries)*100 << "%" << endl;
-        /* OTHER WAY OF DOING IT (without pow)
-        for (int i=0; i<tries; i++) {
-            oddOdds *= (1-odds); // 1 * 0.99 * 0.99 * 0.99 ...
-        }
-        */
         cout << "Do you want to try with other event?: (1 Yes / 0 No): ";
         cin >> r;
     } while (r == 1);
 }
+
+/* Without pow
+int main()
+{
+    float odds;
+    float fOdds = 1;
+    int tries, r;
+    printf("Odds of X (with Y odds) happening after Z tries\n");
+    do {
+        cout << "Odds of X happening (in %): ";
+        cin >> odds;
+        odds = odds/100;
+        cout << "Tries: ";
+        cin >> tries;
+        for (int i=0; i<tries; i++) {
+            fOdds *= (1-odds);
+        }
+        cout << "Odds: " << fOdds*100 << "%" << endl;
+        cout << "Do you want to try with other event?: (1 Yes / 0 No): ";
+        cin >> r;
+    } while (r == 1);
+}
+*/
